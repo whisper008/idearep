46350bcd21a93c03c8ddeb1c67dbf309360922bc
whisper009 <996164275@qq.com>
2018-08-05 18:16:41 +0800

    haha

---
+++ src/main/java/Hello.java
@@ -0,0 +1,26 @@
+import com.sun.net.httpserver.HttpServer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+/**
+ * Created by Administrator on 2018/8/5 0005.
+ */
+public class Hello extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("xixi");
+
+        OutputStream os=resp.getOutputStream();
+        PrintWriter pw=new PrintWriter(os);
+        pw.println("hello xixi");
+        pw.close();
+    }
+
+}
